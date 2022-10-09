195c6c2ef2794ad55bdc71e2dc0994a19fb585ab
Fazry <lhuqita.fazry@gmail.com>
2022-10-09 19:47:34 +0700

    decrease pl version

--- run_train_small.sh
+++ run_train_small.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+mkdir -p lightning_logs/exposure
+
 CUDA_VISIBLE_DEVICES=$CUDA_ID python main.py \
     --data_dir=datasets/Exposure \
     --pretrained=pretrained/swin_small_patch4_window7_224_22k.pth \
     --batch_size=4 \
     --num_workers=4 \
-    --accelerator=cpu \
+    --accelerator=gpu \
     --variant=small \
     --max_epoch=20
