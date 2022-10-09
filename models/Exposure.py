cb5c4cb2e052e93f79833f4c46f02a9234fedf50
Fazry <lhuqita.fazry@gmail.com>
2022-10-09 15:53:30 +0700

    First commit

--- models/Exposure.py
+++ models/Exposure.py
@@ -160,6 +160,7 @@ class Exposure(pl.LightningModule):
         return loss
 
     def validation_step(self, batch, batch_idx):
+        print(f"batch shape: {batch['video'].shape}")
         prediction_labels = self(batch['video'])
        
         loss = F.cross_entropy(prediction_labels['neutral'].sigmoid(), batch['neutral'])
