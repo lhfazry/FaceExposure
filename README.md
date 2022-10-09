ba09a10363d90b65a13f3e54592b3304c3bc8f31
Fazry <lhuqita.fazry@gmail.com>
2022-10-09 13:46:09 +0700

    First commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# UltraSwin
-Follow this [notebook](https://colab.research.google.com/drive/1WYx0TTPkSPSBm9qzfEPC0cB3jiSLt1f2?usp=sharing) for demonstration
--- run_train_small.sh
+++ run_train_small.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 CUDA_VISIBLE_DEVICES=$CUDA_ID python main.py \
-    --data_dir=/workspace/Exposure \
+    --data_dir=/workspace/datasets/Exposure \
     --pretrained=pretrained/swin_small_patch4_window7_224_22k.pth \
     --batch_size=4 \
     --num_workers=4 \
