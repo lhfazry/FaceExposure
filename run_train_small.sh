938c1563bde683d343c044f7135a96dd323c235c
Fazry <lhuqita.fazry@gmail.com>
2022-10-09 13:19:29 +0700

    First commit

--- run_train_small.sh
+++ run_train_small.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-CUDA_VISIBLE_DEVICES=0 python main.py \
+CUDA_VISIBLE_DEVICES=$CUDA_ID python main.py \
     --data_dir=/workspace/Exposure \
     --pretrained=pretrained/swin_small_patch4_window7_224_22k.pth \
     --batch_size=4 \
