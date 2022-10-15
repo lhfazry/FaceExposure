14cebb7690703b02b4ff55fb0e223e229bc284cd
Fazry <lhuqita.fazry@gmail.com>
2022-10-15 19:05:06 +0700

    add crop videos

--- crop_videos.py
+++ crop_videos.py
@@ -88,7 +88,7 @@ def crop_videos(input_dir, output_dir, dim):
         for i in range(frames.shape[0]):
             face = DeepFace.detectFace(img_path = frames[i,:,:,:].squeeze(), 
                 target_size = dim, 
-                detector_backend = 'ssd',
+                detector_backend = 'retinaface',
                 enforce_detection = False
             )
 
