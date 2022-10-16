3217e2f5fbf083f068d81d703fd5cdeb4821ca46
Fazry <lhuqita.fazry@gmail.com>
2022-10-16 13:56:57 +0700

    add crop videos

--- crop_videos.py
+++ crop_videos.py
@@ -172,7 +172,10 @@ def crop_videos2(input_dir, output_dir, dim):
 
         for i in range(frames.shape[0]):
             gray = cv2.cvtColor(frames[i,:,:,:].squeeze(), cv2.COLOR_BGR2GRAY)
-            x, y, w, h = face_cascade.detectMultiScale(gray, 1.3, 5)
+            # x, y, w, h
+            face  = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+            logging.info(f"Finished. Face shape: {face.shape}")
             cropped = frames[y:y+h, x:x+w]
             cropped = image_resize2(cropped, dim)
 
