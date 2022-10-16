55d9c074790a2a6d4ffe01b7dffc7f2878e57a3c
Fazry <lhuqita.fazry@gmail.com>
2022-10-16 21:15:56 +0700

    a

--- run_train_small.sh
+++ run_train_small.sh
@@ -2,11 +2,11 @@
 mkdir -p lightning_logs/exposure
 
 CUDA_VISIBLE_DEVICES=$CUDA_ID python main.py \
-    --data_dir=datasets/Exposure \
+    --data_dir=datasets/cropped2 \
     --pretrained=pretrained/swin_small_patch4_window7_224_22k.pth \
     --batch_size=4 \
     --num_workers=4 \
     --accelerator=gpu \
     --variant=small \
-    --max_epoch=100 \
+    --max_epoch=200 \
     --csv_file=datasets/video_exposure.csv
