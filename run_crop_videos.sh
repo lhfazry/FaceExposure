fd546a1af6fc42e41a26370a8602f79f29a3f6cf
Fazry <lhuqita.fazry@gmail.com>
2022-10-16 13:33:22 +0700

    add crop videos

--- run_crop_videos.sh
+++ run_crop_videos.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 CUDA_VISIBLE_DEVICES=$CUDA_ID python crop_videos.py \
     --input_dir=datasets/Exposure \
-    --output_dir=datasets/cropped \
+    --output_dir=datasets/cropped2 \
     --dim=128
