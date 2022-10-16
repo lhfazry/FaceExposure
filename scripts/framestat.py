4d70de7b02121eace87878250290fcdb85591eec
Fazry <lhuqita.fazry@gmail.com>
2022-10-16 20:15:44 +0700

    a

--- scripts/framestat.py
+++ scripts/framestat.py
@@ -12,7 +12,7 @@ parser.add_argument("--video_dir", type=str, default=None, help="Video directory
 
 params = parser.parse_args()
 
-def load_video(filename: str):
+def get_frame_count(filename: str):
     """Loads a video from a file.
 
     Args:
@@ -33,30 +33,7 @@ def load_video(filename: str):
     capture = cv2.VideoCapture(filename)
 
     frame_count = int(capture.get(cv2.CAP_PROP_FRAME_COUNT))
-    frame_width = int(capture.get(cv2.CAP_PROP_FRAME_WIDTH))
-    frame_height = int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    fps = int(capture.get(cv2.CAP_PROP_FPS))
-
-    v = np.zeros((frame_count, frame_height, frame_width, 3), np.uint8) # (F, H, W, C)
-
-    for count in range(frame_count):
-        ret, frame = capture.read()
-        
-        if not ret:
-            raise ValueError("Failed to load frame #{} of {}.".format(count, filename))
-
-        #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        #frame = cv2.resize(frame, (frame_dim, frame_dim))
-        v[count] = frame
-
-        count += 1
-
-    #capture.release()
-    #v = v.transpose((3, 0, 1, 2)) #(C, F, H, W)
-
-    assert v.size > 0
-
-    return fps, v
+    return frame_count
 
 def calc_frame_stat(dir):
     videos = glob(os.path.join(dir, '*.mp4'))
@@ -64,8 +41,11 @@ def calc_frame_stat(dir):
 
     for video in videos:
         filename = Path(video).name
-        fps, frames = load_video(video)
-        number_of_frames.append(frames[0])
+        frame_count = get_frame_count(video)
+
+        logging.info(f"{filename} : {frame_count}")
+
+        number_of_frames.append(frame_count)
 
     df = pd.DataFrame(number_of_frames)
     df.describe()
