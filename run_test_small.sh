9b3b3012dfd6dc948049c59b2ddff482201f8cc9
Fazry <lhuqita.fazry@gmail.com>
2022-10-18 05:47:58 +0700

    add run test

--- run_test_small.sh
+++ run_test_small.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-CUDA_VISIBLE_DEVICES=0 python main.py \
+CUDA_VISIBLE_DEVICES=$CUDA_ID python main.py \
     --mode=test \
-    --data_dir=/workspace/EchoNet \
-    --ckpt_path=lightning_logs/ultraswin/version_88/checkpoints/epoch\=17-step\=16128.ckpt \
+    --data_dir=datasets/cropped2 \
+    --ckpt_path=$CKPT \
     --batch_size=8 \
     --num_workers=4 \
     --accelerator=gpu \
