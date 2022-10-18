45162b23d87c48e048f487ac8549a04354f99255
Fazry <lhuqita.fazry@gmail.com>
2022-10-18 19:40:51 +0700

    add run test

--- download_dataset.sh
+++ download_dataset.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 mkdir -p datasets/Exposure
-gdown 1h96PnIuzyZLVAgi0SbTNlBg3aR8oAiuR
+gdown 1qQx85P-b0UAu4EJ9H7y3ukLFu6wtO_Ut
 tar -xzf exposure-video.zip --directory datasets/Exposure
 chmod -R 755 datasets/Exposure
