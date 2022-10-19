483843f4d4368e28e45c59685d21301bdcc0d66d
Fazry <lhuqita.fazry@gmail.com>
2022-10-20 04:16:50 +0700

    add more metrics

--- models/Exposure.py
+++ models/Exposure.py
@@ -156,8 +156,8 @@ class Exposure(pl.LightningModule):
         self.log("loss", loss, on_epoch=True, on_step=True, prog_bar=True, logger=False)
         self.log("acc", acc, on_epoch=True, on_step=True, prog_bar=True, logger=False)
 
-        self.logger.experiment.add_scalars('train_val_loss', {'train': loss}, self.global_step) 
-        self.logger.experiment.add_scalars('train_val_acc', {'train': acc}, self.global_step) 
+        self.logger.experiment.add_scalars('train_val_loss', {'train': loss}, global_step=self.current_epoch) 
+        self.logger.experiment.add_scalars('train_val_acc', {'train': acc}, global_step=self.current_epoch) 
 
         return loss
 
@@ -169,8 +169,8 @@ class Exposure(pl.LightningModule):
         self.log("val_loss", loss, on_epoch=True, on_step=True, prog_bar=True, logger=False)
         self.log("val_acc", acc, on_epoch=True, on_step=True, prog_bar=True, logger=False)
 
-        self.logger.experiment.add_scalars('train_val_loss', {'val': loss}, self.global_step) 
-        self.logger.experiment.add_scalars('train_val_acc', {'val': acc}, self.global_step) 
+        self.logger.experiment.add_scalars('train_val_loss', {'val': loss}, global_step=self.current_epoch) 
+        self.logger.experiment.add_scalars('train_val_acc', {'val': acc}, global_step=self.current_epoch) 
         
     def test_step(self, batch, batch_idx):
         prediction_label = self(batch['video'])
