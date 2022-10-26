6683a61f780c9a037b8f5ca1baaf459efd49bac1
Fazry <lhuqita.fazry@gmail.com>
2022-10-27 03:15:00 +0700

    add pretrained

--- run_train_small.sh
+++ run_train_small.sh
@@ -4,13 +4,13 @@ mkdir -p lightning_logs/exposure
 CUDA_VISIBLE_DEVICES=$CUDA_ID python main.py \
     --data_dir=datasets/cropped \
     --pretrained=pretrained/swin_small_patch4_window7_224_22k.pth \
-    --batch_size=4 \
+    --batch_size=8 \
     --num_workers=4 \
     --accelerator=gpu \
     --variant=small \
     --max_epoch=100 \
     --upsampling=1 \
-    --max_frames=1024 \
+    --max_frames=512 \
     --frame_dim=64 \
     --sampling_strategy=truncate \
     --csv_file=datasets/video_exposure_nonetral.csv
