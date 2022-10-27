d648c8c136bd0b84ec4207ccabdafa44da1a7fe6
Fazry <lhuqita.fazry@gmail.com>
2022-10-27 09:39:22 +0700

    add pretrained

--- run_train_base.sh
+++ run_train_base.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
-CUDA_VISIBLE_DEVICES=0 python main.py \
-    --data_dir=/workspace/EchoNet \
-    --pretrained=pretrained/swin_base_patch4_window7_224_22k.pth \
-    --batch_size=2 \
+mkdir -p lightning_logs/exposure
+
+CUDA_VISIBLE_DEVICES=$CUDA_ID python main.py \
+    --data_dir=datasets/cropped \
+    --pretrained=pretrained/swin_base_patch244_window877_kinetics400_22k.pth \
+    --batch_size=4 \
     --num_workers=4 \
     --accelerator=gpu \
     --variant=base \
-    --max_epoch=20
+    --max_epoch=100 \
+    --upsampling=0 \
+    --train_augmentation=0 \
+    --max_frames=512 \
+    --frame_dim=64 \
+    --sampling_strategy=truncate \
+    --csv_file=datasets/video_exposure_nonetral.csv
